824ff1d44ec7b35108719ef7fc03ff52166c1092
Jabal Torres <jabaltorres@gmail.com>
2016-03-10 21:25:27 -0800

    data update

--- js/global.js
+++ js/global.js
@@ -55,14 +55,14 @@ jQuery(function( $ ){
 	// $(".site-inner").css("margin-top", header_height);
 
 	// using gist as a temporary bandaid
-	$.getJSON('https://gist.githubusercontent.com/capitalJT/d2f99864e580533b0c99/raw/b00915a32e3949f752755d81b9f8bc9dc4a888af/jt-data.json', function(data) {
+	$.getJSON('https://gist.githubusercontent.com/capitalJT/d2f99864e580533b0c99/raw/7cf2b4e37fc4d121ba598d8a41df0339b7db975c/jt-data.json', function(data) {
 	    var items = [];
 	  	$.each(data, function(idx, obj){ 
         	$.each(obj, function(key, value){
 
         		// check for external link
         		var aTarget = null;
-				if (value.extLink == true){
+				    if (value.extLink == true){
         			aTarget = 'target="_blank"';
         		} else {
         			aTarget = "";
