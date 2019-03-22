212d9daf8ba95281e562072de9c53e6b4eb029df
Jabal Torres <info@jabaltorres.com>
2019-03-22 14:36:07 -0700

    Updated login page and consolidated scratch files.

--- functions.php
+++ functions.php
@@ -448,6 +448,13 @@ function my_login_logo_url() {
 add_filter( 'login_headerurl', 'my_login_logo_url' );
 
 function my_login_logo_url_title() {
-    return 'Your Site Name and Info';
+    return 'Jabal Torres MuthaFucka!!!';
 }
 add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
+
+function my_login_stylesheet() {
+    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/style-login.css' );
+    wp_enqueue_script( 'custom-login', get_stylesheet_directory_uri() . '/style-login.js' );
+}
+add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
