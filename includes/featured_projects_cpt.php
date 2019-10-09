b355f6fd7843ea89694776b892c9a4227291993d
Jabal Torres <info@jabaltorres.com>
2019-10-08 19:05:13 -0700

    added has-hover class to featured projects

--- includes/featured_projects_cpt.php
+++ includes/featured_projects_cpt.php
@@ -4,12 +4,12 @@
 //    $project_button_text = get_field('project_button_text');
 ?>
 
-<article class="featured-project-list-item border p-4 mb-4">
+<article class="featured-project-list-item border has-hover p-4 mb-4">
   
     <a href="<?php echo get_permalink(); ?>"><h1 class="article-title"><?php echo the_title();?></h1></a>
 
 	<?php if ( has_post_thumbnail() ) : ?>
-        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="fp-thumbnail">
             <img class="thumbnail mb-2" src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_post_thumbnail_caption(); ?>" />
         </a>
 	<?php endif; ?>
